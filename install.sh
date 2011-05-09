f47c0280554e801b49bb4c924551fb27dd6bfa35
Michael Carroll <carroll.michael@gmail.com>
2011-05-09 13:48:13 -0500

    More moving around

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+function link_file {
+    source="${PWD}/$1"
+    target="${HOME}/${1/_/.}"
+
+    if [ -e "${target}" ]; then
+        mv $target $target.bak
+    fi
+
+    ln -sf ${source} ${target}
+}
+
+if [ "$1" = "vim" ]; then
+    for i in _vim*
+    do
+       link_file $i
+    done
+else
+    for i in _*
+    do
+        link_file $i
+    done
+fi
+
+git submodule sync
+git submodule init
+git submodule update
+git submodule foreach git pull origin master
+git submodule foreach git submodule init
+git submodule foreach git submodule update
+
+# setup command-t
+cd _vim/bundle/command-t
+rake make
