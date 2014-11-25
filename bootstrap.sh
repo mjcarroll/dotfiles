6a65cba0883e456b81c83cddc9d044d61306c45e
Michael Carroll <carroll.michael@gmail.com>
2014-11-25 10:17:20 -0600

    Adding Ansible.

---
+++ bootstrap.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if [ ! -e ~/src/dotfiles ]; then
+sudo apt-get update
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get install -y ansible
+
+sudo cat <<EOT > /etc/ansible/hosts
+[local]
+localhost
+EOT
+fi
+
+if [ ! -e ~/src/dotfiles ]; then
+  mkdir ~/src
+  git clone --recursive https://github.com/mjcarroll/dotfiles ~/src/dotfiles
+fi
+
+ansible-playbook ~/src/dotfiles/ansible/playbook.yml -c local -K
